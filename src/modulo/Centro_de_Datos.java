15d79aacd8fe6782c3dacbe98184c30852c45d53
Xabier <xabieriglesiasmartinez@gmail.com>
2026-03-04 10:37:57 +0100

    Completado los Getters y Setters de la clase Centro_de_Datos

--- src/modulo/Centro_de_Datos.java
+++ src/modulo/Centro_de_Datos.java
@@ -24,49 +24,62 @@ public class Centro_de_Datos {
 	
 	}
 	
-	
-	
-	
 	// Getters y Setters (Necesarios para acceder  a cada dato)
 
-    public String getUbicacion() {
-		return ubicacion;
+	public String getNombre() {
+		return nombre;
 	}
 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-
+	
+	public String getUbicacion() {
+		return ubicacion;
+	}
 
 	public void setUbicacion(String ubicacion) {
 		this.ubicacion = ubicacion;
 	}
 
+	
+	public String getPais() {
+		return pais;
+	}
 
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
 
-
+	
 	public LocalDate getFecha_apertura() {
 		return fecha_apertura;
 	}
-
-
-
 
 	public void setFecha_apertura(LocalDate fecha_apertura) {
 		this.fecha_apertura = fecha_apertura;
 	}
 
-
-
-
+	
 	public int getSuperficie_m2() {
 		return superficie_m2;
 	}
 
-
-
-
 	public void setSuperficie_m2(int superficie_m2) {
 		this.superficie_m2 = superficie_m2;
 	}
+
+	
+	public int getNum_servidores() {
+		return num_servidores;
+	}
+
+	public void setNum_servidores(int num_servidores) {
+		this.num_servidores = num_servidores;
+	}
+	
+	
 }
 
 
